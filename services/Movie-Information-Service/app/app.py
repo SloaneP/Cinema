a0ecb720d1bbfb08c9ddffe084132fd269047c49
SloaneP <serezha-fedorov.01@mail.ru>
2023-11-05 02:17:08 +0300

    Update

--- services/Movie-Information-Service/app/app.py
+++ services/Movie-Information-Service/app/app.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from typing import List, Tuple
 from .schemas import MovieCreate, MovieUpdate, Movie
-from app import crud
+from . import crud, config
+from .database.database import MongoDB
 from uuid import UUID
+
+cfg: config.Config = config.load_config()
+MongoDB(mongo_dsn=cfg.mongo_dsn)
 
 app = FastAPI(
     title="Movie Information Service"
@@ -25,14 +29,14 @@ def get_movies(limit: int = 10, offset: int = 0) -> Tuple[int, List[Movie]]:
     return crud.get_movies(limit, offset)
 
 @app.put("/movies/{movie_id}", response_model=Movie)
-def update_movie(movie_id: str, movie_update: MovieUpdate) -> Movie:
+def update_movie(movie_id: UUID, movie_update: MovieUpdate) -> Movie:
     movie = crud.update_movie(movie_id, movie_update)
     if movie is None:
         return JSONResponse(status_code=404, content={"message": "Movie not found"})
     return movie
 
 @app.delete("/movies/{movie_id}", response_model=Movie)
-def delete_movie(movie_id: str) -> Movie:
+def delete_movie(movie_id: UUID) -> Movie:
     movie = crud.delete_movie(movie_id)
     if movie is None:
         return JSONResponse(status_code=404, content={"message": "Movie not found"})
--- services/Movie-Information-Service/app/crud.py
+++ services/Movie-Information-Service/app/crud.py
@@ -1,43 +1,36 @@
 from typing import List, Tuple
 from .schemas import MovieCreate, MovieUpdate, Movie
+from app.database.models import Movie as MovieModel
 from uuid import UUID
 
-movies = []
-next_movie_id = 1
-
 def create_movie(movie: MovieCreate) -> Movie:
-    global next_movie_id
     movie_data = movie.dict()
-    movie_id = UUID(int=next_movie_id)  # Генерируем UUID из целочисленного значения
-    next_movie_id += 1
-    new_movie = Movie(id=movie_id, **movie_data)
-    movies.append(new_movie)
+    new_movie = MovieModel(**movie_data)
+    new_movie.save()
     return new_movie
 
 def get_movie(movie_id: UUID) -> Movie:
-    for movie in movies:
-        if movie.id == movie_id:
-            return movie
-    return None
+    movie = MovieModel.objects(id=movie_id).first()
+    return movie
 
 def get_movies(limit: int = 10, offset: int = 0) -> Tuple[int, List[Movie]]:
-    movie_list = movies[offset:offset + limit]
-    movies_count = len(movies)
-    return movies_count, movie_list
+    movies = MovieModel.objects.skip(offset).limit(limit)
+    movies_count = MovieModel.objects.count()
+    return movies_count, movies
 
 def update_movie(movie_id: UUID, movie_update: MovieUpdate) -> Movie:
-    for movie in movies:
-        if movie.id == movie_id:
-            update_data = movie_update.dict(exclude_unset=True)
-            updated_movie = movie.copy(update=update_data)
-            movies.remove(movie)
-            movies.append(updated_movie)
-            return updated_movie
+    movie = MovieModel.objects(id=movie_id).first()
+    if movie:
+        update_data = movie_update.dict(exclude_unset=True)
+        for field, value in update_data.items():
+            setattr(movie, field, value)
+        movie.save()
+        return movie
     return None
 
 def delete_movie(movie_id: UUID) -> Movie:
-    for movie in movies:
-        if movie.id == movie_id:
-            movies.remove(movie)
-            return movie
+    movie = MovieModel.objects(id=movie_id).first()
+    if movie:
+        movie.delete()
+        return movie
     return None
