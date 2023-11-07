0f01d944fbce711a60e0ae65fb2282559e303985
SloaneP <serezha-fedorov.01@mail.ru>
2023-11-07 22:37:33 +0300

    Update

--- services/Movie-Information-Service/app/database/models.py
+++ services/Movie-Information-Service/app/database/models.py
@@ -1,11 +1,14 @@
-from mongoengine import Document, StringField, IntField, ListField
+from mongoengine import Document, StringField, IntField, ListField, UUIDField
+from uuid import uuid4
+from bson import ObjectId
 
 class Movie(Document):
+    id = UUIDField(primary_key=True, default=uuid4, binary=False)
     title = StringField(required=True)
-    description = StringField()
-    genre = StringField()
-    actors = ListField(StringField())
-    release_year = IntField()
+    description = StringField(required=True)
+    genre = StringField(required=True)
+    actors = ListField(StringField(), required=True)
+    release_year = IntField(required=True)
 
     def __str__(self):
-        return f"Movie(title={self.title}, description={self.description}, genre={self.genre}, actors={self.actors}, release_year={self.release_year})"
+        return f"Movie(_id={str(self.id)}, title={self.title}, description={self.description}, genre={self.genre}, actors={self.actors}, release_year={self.release_year})"
