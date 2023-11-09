c08ffd55111394c67cc6f0667930057c88e19a7a
SloaneP <serezha-fedorov.01@mail.ru>
2023-11-09 22:59:40 +0300

    Update crud.py

--- services/Movie-Information-Service/app/crud.py
+++ services/Movie-Information-Service/app/crud.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from .schemas import MovieCreate, MovieUpdate, Movie
 from app.database.models import Movie as MovieModel
 from uuid import UUID
@@ -9,26 +9,26 @@ def create_movie(movie: MovieCreate) -> Movie:
     new_movie.save()
     return new_movie
 
-def get_movie(movie_id: UUID) -> Movie:
+def get_movie(movie_id: UUID) -> Optional[Movie]:
     movie = MovieModel.objects(id=movie_id).first()
     return movie
 
 def get_movies(limit: int = 10, offset: int = 0) -> Tuple[int, List[Movie]]:
     movies = MovieModel.objects.skip(offset).limit(limit)
     movies_count = MovieModel.objects.count()
-    return movies_count, movies
+    return movies_count, list(movies)
 
-def update_movie(movie_id: UUID, movie_update: MovieUpdate) -> Movie:
+def update_movie(movie_id: UUID, movie_update: MovieUpdate) -> Optional[Movie]:
     movie = MovieModel.objects(id=movie_id).first()
     if movie:
-        update_data = movie_update.dict(exclude_unset=True)
+        update_data = movie_update.model_dump(exclude_unset=True)
         for field, value in update_data.items():
             setattr(movie, field, value)
         movie.save()
         return movie
     return None
 
-def delete_movie(movie_id: UUID) -> Movie:
+def delete_movie(movie_id: UUID) -> Optional[Movie]:
     movie = MovieModel.objects(id=movie_id).first()
     if movie:
         movie.delete()
